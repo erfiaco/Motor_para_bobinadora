22e83f1cab3e2ae1fbfd0dcb30f7a2acef14c439
Javo <erfiaco@gmail.com>
2024-11-19 11:14:15 +0100

    añadimos finally cleanup para dar robustez

--- Motor.py
+++ Motor.py
@@ -1,30 +1,29 @@
-'''Añadimos la función de stop separadamente'''
-
 import RPi.GPIO as GPIO
 import time
 
-EN  = 4
+EN = 4
 IN1 = 17
 IN2 = 27
 
 def setup():
-    
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup([EN,IN1,IN2],GPIO.OUT)
+    GPIO.setup([EN, IN1, IN2], GPIO.OUT)
 
 def endavant(t):
-    
     GPIO.output(EN, GPIO.HIGH)
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
     time.sleep(t)
- 
+
 def stop(t):
-    
     GPIO.output(EN, GPIO.LOW)
-    time.sleep(t)
-    
-setup()
-endavant(5)
-stop(5)
-GPIO.cleanup()
+    time.sleep(2)
+
+try:
+    setup()
+    t = float(input("Introduce el tiempo en segundos para el movimiento: "))
+    endavant(t)
+    stop(t)
+finally:
+    GPIO.cleanup()
+    print("Limpieza de GPIO completada.")
